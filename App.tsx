150700df80609f4dd48b345c972bb08de42c981e
unknown <PCTIEI@PCTIEI-2.csu.local>
2026-08-26 17:32:34 -0300

    Primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+import { Loginscreen } from './src/screens/login';
+
+const imagemFundo = require('./src/assets/imgs/background.png');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={imagemFundo} style={styles.background}>
+      <View style={styles.container}>
+        <Loginscreen />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
 });
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+
+export interface LoginscreenProps {
+}
+
+export function Loginscreen(props: LoginscreenProps) {
+  return (
+    <ImageBackground source={require('../../assets/imgs/background.png')} style={styles.background}>
+      <View style={styles.container}>
+        <Text style={styles.logo}>APP</Text>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: { flex: 1,width: '100%', height: '100%'},
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center'},
+  logo: {color: 'white', fontSize: 50,textAlign: 'center',
+  },
+});
